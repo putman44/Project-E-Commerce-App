5f0d32ceca4aa7c2cc7beb2a43708b9c05af6125
Taylor Putman <fishputman@gmail.com>
2025-06-05 08:19:14 -0400

    added products state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Camping Backpack",
+      price: 159.99,
+      description:
+        "A durable and spacious backpack for all your camping needs.",
+    },
+    {
+      id: 2,
+      name: "Camping Tent",
+      price: 100.0,
+      description:
+        "A lightweight and easy-to-set-up tent for outdoor adventures.",
+    },
+    {
+      id: 3,
+      name: "Cooking stove",
+      price: 49.99,
+      description: "A portable stove for cooking meals in the wild.",
+    },
+  ]);
 
   return (
     <>
@@ -29,7 +50,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
