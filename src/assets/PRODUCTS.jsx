3fde9fe2b02629529515d1009fa141cf71c44788
Taylor Putman <fishputman@gmail.com>
2025-06-05 12:50:09 -0400

    added media queries and new images

--- src/assets/PRODUCTS.jsx
+++ src/assets/PRODUCTS.jsx
@@ -2,6 +2,7 @@ const PRODUCTS = [
   {
     id: 1,
     name: "Camping Backpack",
+    category: "Backpacks",
     price: 159.99,
     description:
       "A durable and spacious backpack for all your camping needs. It features multiple compartments and is made from water-resistant material.",
@@ -11,7 +12,8 @@ const PRODUCTS = [
   {
     id: 2,
     name: "Camping Tent",
-    price: 100.0,
+    category: "Tents",
+    price: 199.99,
     description:
       "A lightweight and easy-to-set-up tent for outdoor adventures.",
     image:
@@ -20,6 +22,7 @@ const PRODUCTS = [
   {
     id: 3,
     name: "Cooking stove",
+    category: "Cooking Gear",
     price: 49.99,
     description: "A portable stove for cooking meals in the wild.",
     image:
@@ -28,47 +31,52 @@ const PRODUCTS = [
   {
     id: 4,
     name: "Camping Chair",
+    category: "Camping Gear",
     price: 39.99,
     description:
       "A comfortable and foldable chair for relaxing around the campfire.",
     image:
-      "https://images.unsplash.com/photo-1599711862405-c7068b48ee32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1664287397675-81eee53d2819?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 5,
     name: "Sleeping Bag",
+    category: "Camping Gear",
     price: 59.99,
     description:
       "A warm and cozy sleeping bag for a good night's sleep under the stars.",
     image:
-      "https://images.unsplash.com/photo-1599711862405-c7068b48ee32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1486999619268-6aa409dbecd1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 6,
     name: "Camping Lantern",
+    category: "Camping Gear",
     price: 29.99,
     description:
       "A bright and energy-efficient lantern for lighting up your campsite.",
     image:
-      "https://images.unsplash.com/photo-1599711862405-c7068b48ee32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1679392537256-80537b91b02d?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 7,
     name: "First Aid Kit",
+    category: "Camping Gear",
     price: 24.99,
     description:
       "A comprehensive first aid kit for treating minor injuries while camping.",
     image:
-      "https://images.unsplash.com/photo-1599711862405-c7068b48ee32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1624638760852-8ede1666ab07?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 8,
     name: "Camping Cookware Set",
+    category: "Cooking Gear",
     price: 34.99,
     description:
       "A lightweight and compact cookware set for preparing meals outdoors.",
     image:
-      "https://images.unsplash.com/photo-1599711862405-c7068b48ee32?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1661393013602-a208f7118cbd?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
--- src/components/Product-List.jsx
+++ src/components/Product-List.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
 import ProductItem from "./Product-Item";
 import styles from "./Product-List.module.css";
 
 const ProductList = ({ products }) => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const productsByCategory = products.reduce((acc, product) => {
+    if (!acc[product.category]) {
+      acc[product.category] = [];
+    }
+    acc[product.category].push(product);
+    return acc;
+  }, {});
+
+  const handleFilterChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : productsByCategory[selectedCategory] || [];
+
+  let styleProductList;
+
+  if (filteredProducts.length === 1) {
+    styleProductList = styles.singleProductList;
+  } else {
+    styleProductList = styles.productList;
+  }
+
   return (
     <>
       <h1>Product List</h1>
-      <div className={styles.productList}>
-        {products.map((product) => {
+      <div className={styles.categorySelect}>
+        <label htmlFor="filter">Filter by Category: </label>
+        <select onChange={handleFilterChange} name="filter" id="filter">
+          <option value="All">All</option>
+          {Object.keys(productsByCategory).map((category) => (
+            <option key={category} value={category}>
+              {category.charAt(0).toUpperCase() + category.slice(1)}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className={styleProductList}>
+        {filteredProducts.map((product) => {
           return <ProductItem key={product.id} product={product} />;
         })}
       </div>
